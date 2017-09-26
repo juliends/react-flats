1f342e5e8b332f54cafa58dd047a6cf49b446118
Julien Da Silva <julien.da.silva@hotmail.com>
2017-09-26 11:22:59 +0200

    flats data

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -7,13 +7,11 @@ class Flat extends Component {
 
   render(){
     return (
-      <div className="card">
-        <div className="card-category">Popular</div>
+      <div className="card" >
+        <div className="card-category">TODO props.price</div>
         <div className="card-description">
           <h2>Le Wagon Shanghai</h2>
-          <p>Very cool city, the best</p>
         </div>
-        <img className="card-user" src="https://kitt.lewagon.com/placeholder/users/tgenaitay"/>
         <a className="card-link" href="#" ></a>
       </div>
     )
